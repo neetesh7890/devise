4a47e9d585a338569617afc5fe906ea2b99d6fa0
dev08 <gitk>
2017-12-08 15:01:42 +0530

    contact gem removed

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,14 +4,14 @@ class DashboardsController < ApplicationController
     @contacts = request.env['omnicontacts.contacts']
   end
 
-  def contact_callback
-    @contacts = request.env['omnicontacts.contacts']
-    @user = request.env['omnicontacts.user']
-    puts "List of contacts of #{@user[:name]} obtained from #{params[:importer]}:"
-    @contacts.each do |contact|
-      puts "Contact found: name => #{contact[:name]}, email => #{contact[:email]}"
-    end
-  end
+  # def contact_callback
+  #   @contacts = request.env['omnicontacts.contacts']
+  #   @user = request.env['omnicontacts.user']
+  #   puts "List of contacts of #{@user[:name]} obtained from #{params[:importer]}:"
+  #   @contacts.each do |contact|
+  #     puts "Contact found: name => #{contact[:name]}, email => #{contact[:email]}"
+  #   end
+  # end
 
   def failure
   end
